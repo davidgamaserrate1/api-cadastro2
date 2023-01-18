316e22d3afce03edad60f7e1c791e65bdb39bbff
davidgamaserrate1 <david.gama@ufms.br>
2023-01-17 21:24:41 -0300

    tring deploy

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 import express from "express"
 import db from "./config/dbConnect.js"
-import clients from "./models/Client.js"
-import router from "./routes/clientesRoutes.js"
 import routes from "./routes/index.js"
 
 db.on("error",console.log.bind(console,'Conection Error X.X'))
